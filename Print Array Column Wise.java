fff06e9a411486eb2ec01e234b43039bb2f84670
Siddhant Sunil Meshram <siddhantmeshram149@gmail.com>
2021-01-17 19:17:33 +0530

    Add files via upload

---
+++ Print Array Column Wise.java
@@ -0,0 +1,19 @@
+class Array1
+{
+public static void main(String args[])
+{
+int i,j,k=0;
+int[][] a={
+	   {45,23},
+           {34,53},
+           {23,12},
+                  };
+for(i=0;i<=2;i++)
+{for(j=0;j<=1;j++)
+{k=a[i][j];
+System.out.print(k+" ");
+}
+System.out.println(" ");
+}
+}
+}
