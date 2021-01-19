8aa78d4fc9b14d0536badaa7a5770110d011cf55
Siddhant Sunil Meshram <siddhantmeshram149@gmail.com>
2021-01-19 19:32:37 +0530

    Add files via upload

---
+++ Bit Shifting.java
@@ -0,0 +1,11 @@
+class bit
+{
+public static void main(String args[])
+{
+int a=5,b,c;
+b=a<<3;
+c=a>>3;
+System.out.println("After left shift:"+b);
+System.out.println("After right shift:"+c);
+}
+}
