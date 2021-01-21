e760fcd88cf8694f4771abe141b62ad91e8a1802
Siddhant Sunil Meshram <siddhantmeshram149@gmail.com>
2021-01-21 23:16:51 +0530

    Add files via upload

---
+++ Logical Operators.java
@@ -0,0 +1,12 @@
+class bit1
+{
+public static void main(String args[])
+{
+boolean a=true,b=false;
+System.out.println("Logical OR:"+(a|b));
+System.out.println("Logical AND:"+(a&b));
+System.out.println("Logical XOR:"+(a^b));
+System.out.println("Logical NOT:"+(!a));
+System.out.println("Logical NOT:"+(!b));
+}
+}
