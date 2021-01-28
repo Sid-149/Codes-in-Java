19e296c3f538a2ca576bb6e311af02cc30a1be77
Siddhant Sunil Meshram <siddhantmeshram149@gmail.com>
2021-01-28 23:13:27 +0530

    Add files via upload

---
+++ Circle.java
@@ -0,0 +1,20 @@
+class Circle
+{
+public static void main(String args[])
+{
+int r=10;
+double aoc,coc;
+aoc=3.14*r*r;
+coc=2*3.14*r;
+System.out.println("AREA OF CIRCLE:"+aoc);
+System.out.println("CIRCUMFERENCE OF CIRCLE:"+coc);
+}
+}
+/*
+OUTPUT:
+C:\Users\exam1\Desktop\Siddhant extc>javac Circle.java
+
+C:\Users\exam1\Desktop\Siddhant extc>java Circle
+AREA OF CIRCLE:314.0
+CIRCUMFERENCE OF CIRCLE:62.800000000000004
+*/
