a66744f59bc9387d46e70ae9428c15ebf3918e92
Siddhant Sunil Meshram <siddhantmeshram149@gmail.com>
2021-02-04 22:10:36 +0530

    Add files via upload

---
+++ Degree.java
@@ -0,0 +1,11 @@
+class Degree
+{
+public static void main(String args[])
+{
+double c=10,f;
+f=9/5*(32+c);
+System.out.println("Temperature in Celcius:"+c);
+
+System.out.println("Temperature in Farenheit:"+f);
+}
+}
